304187acff349b2fb31c5077454b1906fc59b738
naliali <nalialiangela365@gmail.com>
2024-03-23 07:52:04 +0300

    connection

---
+++ studentGrade.js
@@ -0,0 +1,18 @@
+// A > 79, B - 60 to 79, C -  49 to 59, D - 40 to 49, E - less 40
+function userGrade(userInput){
+    let finalGrade = "E";
+    if(userInput >= 0 && userInput < 40){
+        finalGrade = "E";
+    }else if(userInput >= 40 && userInput <49){
+        finalGrade = "D";
+    }else if(userInput >= 49 && userInput < 59){
+        finalGrade = "C";
+    }else if(userInput >= 60 && userInput < 79){
+        finalGrade = "B";
+    }else if(userInput >= 80 && userInput <100){
+        finalGrade = "A";
+    }
+    return finalGrade;
+    console.log(finalGrade);
+
+}
